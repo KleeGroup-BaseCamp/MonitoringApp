5ab742acf28e6b2d6f2fea3fe9a57e2f1728dd2f
jguerin <julian.guerin@gmail.com>
2013-11-22 20:51:28 +0100

    Auto retrieve the data when instantiating a sensor

--- app/models/sensor-model.js
+++ app/models/sensor-model.js
@@ -2,6 +2,15 @@ module.exports = SensorModel = Backbone.Model.extend({
 	idAttribute: 'sensor_id',
 	defaults : {
 		room : 'Kitchen',
+		data : {
+			value : 0
+		}
 	},
-	
+	initialize : function(){
+		if(!this.get('data')){
+			console.log(this.get('model'));
+			console.log("Fetching data for new model");
+			this.fetch();
+		}
+	}
 });
