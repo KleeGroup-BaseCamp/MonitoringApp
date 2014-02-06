dca14ed06c86c16bc4959e4ab563ecd683e19b7a
jguerin <julian.guerin@gmail.com>
2014-02-06 02:48:51 +0100

    Fix url for room-collection

--- app/models/room-collection.js
+++ app/models/room-collection.js
@@ -1,7 +1,7 @@
 RoomModel = require('models/room-model');
 
 module.exports = RoomCollection = Backbone.Collection.extend({
-    url: 'http://127.0.0.1:4000/room/',
+    url: 'http://127.0.0.1:4000/rooms/',
     model: RoomModel
 
 });
